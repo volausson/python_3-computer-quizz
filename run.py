8278a5fe9112a06236faf796f9d9302734b54c9b
volausson <veronicagolausson@gmail.com>
2022-08-30 18:17:56 +0000

    Add questions to run.py

--- run.py
+++ run.py
@@ -1,3 +1,53 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Questions for Quizz
+"""
+
+questions = [
+
+    {
+        'question': 'What does programming mean?',
+        "options": [
+          'a) To give instructions to a computer',
+          'b) Play difficult games on the computer',
+          'c) To build computers',
+        ],
+        'correct_answer': 'a'
+    },
+    {
+        'question': 'In computing there are 8 bits to a byte, but what are 4 bits called?',
+        'options': [
+           "a) A nibble",
+           "b) A nipple",
+           "c) A nizzle"
+        ],
+        'correct_answer': 'a'
+    },
+    {
+        'question': 'What is an algorithm?',
+        'options': [
+             'a) A robot',
+             'b) A series of instructions',
+             'c) A mathematical problem'
+        ],
+        'correct_answer': 'b'
+    },
+    {
+        'question': 'What sort of animal is Tux, the official mascot of the Linux operating system?',
+
+        'options': [
+            'a) A fox',
+            'b) A penguin',
+            'c) A parrot'
+        ],
+        'correct_answer': 'b'
+    },
+    {
+        'question': "What was the name of the world's first programmer?",
+        'options': [
+            'a) Adam Lovelace',
+            'b) Ana Lovelace',
+            'c) Ada Lovelace'
+        ],
+        'correct_answer': 'c'
+    },
+]
