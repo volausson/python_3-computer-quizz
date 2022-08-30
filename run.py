432dc42ae169b63ca9c618e3657526f714900b2b
volausson <veronicagolausson@gmail.com>
2022-08-30 18:55:54 +0000

    Fix 'line too long' in problems

--- run.py
+++ run.py
@@ -14,7 +14,8 @@ questions = [
         'correct_answer': 'a'
     },
     {
-        'question': 'In computing there are 8 bits to a byte, but what are 4 bits called?',
+        'question': 'In computing there are 8 bits to a byte,'
+        'but what are 4 bits called?',
         'options': [
            "a) A nibble",
            "b) A nipple",
@@ -24,6 +25,7 @@ questions = [
     },
     {
         'question': 'What is an algorithm?',
+
         'options': [
              'a) A robot',
              'b) A series of instructions',
@@ -32,7 +34,8 @@ questions = [
         'correct_answer': 'b'
     },
     {
-        'question': 'What sort of animal is Tux, the official mascot of the Linux operating system?',
+        'question': 'What sort of animal is Tux,'
+        'the official mascot of the Linux operating system?',
 
         'options': [
             'a) A fox',
@@ -63,11 +66,14 @@ def start_quizz():
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     ask = True
     while ask:
-        question = input('Are you ready to play?\nPlease enter y for yes or n for no:\n').lower().strip()
+        question = input('Are you ready to play?\n'
+                         'Please enter y for yes or'
+                         'n for no:\n').lower().strip()
         if question == 'y':
             enter_quizz()
         if question == '':
-            print('You need to type y for yes to play, or n for no to exit game!\n')
+            print('You need to type y for yes to play,'
+                  'or n for no to exit game!\n')
         if question == 'n':
             print('~~~~~~~~~~~~~~~~~~~~~~')
             print('Okey, maybe next time.')
@@ -80,7 +86,9 @@ def enter_quizz():
     """
     Function to engage user in quizz and give responsive feedback.
     """
-    player_name = input('Please enter your name, otherwise I will just call you Player:\n').lower().strip()
+    player_name = input('Please enter your name,'
+                        'otherwise I will just call you'
+                        'Player:\n').lower().strip()
     if player_name == '':
         player_name = 'Player'
     print(f'Hi, {player_name}, here comes your first question...')
