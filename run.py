6885161381ff6ef49d6e8e261797f0d9aab55646
volausson <veronicagolausson@gmail.com>
2022-08-31 18:15:46 +0000

    Correct spelling in run.py

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ questions = [
         'correct_answer': 'a'
     },
     {
-        'question': 'In computing there are 8 bits to a byte,'
+        'question': 'In computing there are 8 bits to a byte, '
         'but what are 4 bits called?',
         'options': [
            "a) A nibble",
@@ -34,7 +34,7 @@ questions = [
         'correct_answer': 'b'
     },
     {
-        'question': 'What sort of animal is Tux,'
+        'question': 'What sort of animal is Tux, '
         'the official mascot of the Linux operating system?',
 
         'options': [
@@ -67,12 +67,12 @@ def start_quizz():
     ask = True
     while ask:
         question = input('Are you ready to play?\n'
-                         'Please enter y for yes or'
+                         'Please enter y for yes or '
                          'n for no:\n').lower().strip()
         if question == 'y':
             enter_quizz()
         if question == '':
-            print('You need to type y for yes to play,'
+            print('You need to type y for yes to play, '
                   'or n for no to exit game!\n')
         if question == 'n':
             print('~~~~~~~~~~~~~~~~~~~~~~')
@@ -86,8 +86,8 @@ def enter_quizz():
     """
     Function to engage user in quizz and give responsive feedback.
     """
-    player_name = input('Please enter your name,'
-                        'otherwise I will just call you'
+    player_name = input('Please enter your name, '
+                        'otherwise I will just call you '
                         'Player:\n').lower().strip()
     if player_name == '':
         player_name = 'Player'
